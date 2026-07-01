d9e4a65c5253c844301414533eb025b6d4e24200
Mohit Kunwar <mohitkunwar919@gmail.com>
2026-07-01 11:08:45 +0530

    Add files via upload

---
+++ 05-Twopointers-SlidingWindow/largestsubarray.cpp
@@ -0,0 +1,81 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int n;
+// int maxcount =0;
+// int sum = 0;
+// int i = 0;
+// int j = 0;
+// int limit;
+// cout<<"enter the size of the array: ";
+// cin>>n;
+// int arr[n];
+// for(int i =0;i<n;i++){
+//     cout<<"enter the elemets of the array: ";
+//     cin>>arr[i];
+// }
+// cout<<"enter the limit of the sum of the subarray: ";
+// cin>>limit;
+// while(j<n){
+//     sum += arr[j];
+//     j++;
+//     while(sum>limit){
+//         sum -= arr[i];
+//         i++;
+//     }
+//     maxcount = max(j-i,maxcount);
+
+// }
+// cout<<maxcount;
+
+
+// return 0;
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+int n;
+int i = 0;
+int j = 0;
+int sum = 0;
+int maximum_sum = 0;
+cout<<"enter the size of the array: ";
+cin>>n;
+int arr[n];
+// for(int k = 0;k<n;k++){
+//     cout<<"enter the elements of the array: ";
+//     cin>>arr[k];
+// }
+// for(int i = 0;i<=n-3;i++){
+//     sum = 0;
+//     for(int j = i;j<i+3;j++){
+//         sum += arr[j];
+//     }
+//     maximum_sum = max(maximum_sum,sum);
+//     cout<<endl;
+// }
+while (j < n) {
+
+        // Expand the window
+        sum += arr[j];
+
+        // Window size is less than k
+        if (j - i + 1 < 3) {
+            j++;
+        }
+
+        // Window size becomes exactly k
+        else if (j - i + 1 == 3) {
+
+            // Process the current window
+            maximum_sum = max(maximum_sum, sum);
+
+            // Slide the window
+            sum -= arr[i];
+            i++;
+            j++;
+        }
+    }
+return 0;
+}
