8653f5f42d5256e62cdb990088ed3bb6d97a19b1
Mohit Kunwar <mohitkunwar919@gmail.com>
2026-07-02 22:44:08 +0530

    Refactor string functions and add new features
    Refactored string manipulation functions and added new features.

--- 03-strings/stringpractice.cpp
+++ 03-strings/stringpractice.cpp
@@ -1,96 +1,132 @@
 #include<iostream>
 using namespace std;
 char* create(){
-    char* str = new char[100];
+    char *str = new char[100];
     cout<<"enter your string: ";
     cin.getline(str,100);
     return str;
 }
-int StringLength(char str[]){
-    int count =0;
-    for(int i = 0;str[i] != '\0';i++){
+int string_length(char str[]){
+    int count = 0;
+    for(int i = 0 ; str[i] != '\0';i++){
         count++;
     }
-    return count;
+    cout<<count;
 }
 void Upper_Lower(char str[]){
-    for(int i = 0 ;str[i]!='\0';i++){
-        str[i]+= 32;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        str[i]+=32;
     }
-    cout<<str;
 }
-void Toggle(char str[]) {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] >= 'a' && str[i] <= 'z')
-            str[i] = str[i] - 32;
-        else if (str[i] >= 'A' && str[i] <= 'Z')
-            str[i] = str[i] + 32;
+void Toggle(char str[]){
+    for(int i = 0; str[i] != '\0'; i++){
+        if(str[i] >= 'a' && str[i] <= 'z')
+            str[i] -= 32;
+        else if(str[i] >= 'A' && str[i] <= 'Z')
+            str[i] += 32;
     }
-    cout<<str;
 }
-#include<iostream>
-using namespace std;
-char* create(){
-    char* str = new char[100];
-    cout<<"enter your string: ";
-    cin.getline(str,100);
-    return str;
-}
-int StringLength(char str[]){
-    int count =0;
-    for(int i = 0;str[i] != '\0';i++){
-        count++;
+void Display(char str[]){
+    for(int i = 0;str[i] !='\0';i++){
+        cout<<str[i];
     }
-    return count;
 }
-void Upper_Lower(char str[]){
-    for(int i = 0 ;str[i]!='\0';i++){
-        str[i]+= 32;
-    }
-    cout<<str;
-}
-void Toggle(char str[]) {
-    for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] >= 'a' && str[i] <= 'z')
-            str[i] = str[i] - 32;
-        else if (str[i] >= 'A' && str[i] <= 'Z')
-            str[i] = str[i] + 32;
-    }
-    cout<<str;
-}
-void Count_Vowel(char str[])
-{
+int Count_Vowel(char str[]){
+
     int vcount = 0;
     int ccount = 0;
 
-    char vowel[] = {'A','E','I','O','U','a','e','i','o','u','\0'};
+    for(int i = 0; str[i] != '\0';i++){
+        if(str[i]=='A'|| str[i]=='a'||
+            str[i]=='E' || str[i]=='e'||
+            str[i]=='O'|| str[i]=='o' ||
+            str[i]=='I' || str[i]=='i'||
+            str[i]=='U' || str[i]=='u'){
 
-    for(int i = 0; str[i] != '\0'; i++)
-    {
-        bool found = false;
-
-        for(int j = 0; vowel[j] != '\0'; j++)
-        {
-            if(str[i] == vowel[j])
-            {
                 vcount++;
-                found = true;
-                break;
             }
-        }
-
-        if(!found && ((str[i] >= 'A' && str[i] <= 'Z') ||
-                      (str[i] >= 'a' && str[i] <= 'z')))
-        {
+        else if((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')){
             ccount++;
         }
     }
+    return vcount;
+}
+void reverse(char str[]){
+    int i = 0;
+    int j = string_length(str) -1;
+    while(i<j){
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    i++;
+    j--;
+    }
+}
+bool is_palindrome(char str[]){
+    int i = 0;
+    int j = string_length(str) -1;
+    while(i<j){
+        if(str[i] != str[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+        return true;
+}
+bool is_Anagram(char str1[],char str2[]){
+    int H[26]={0};
+    int k[26]={0};
+    
+    for(int i = 0 ; str1[i] !='\0' ; i++){
+        H[str1[i]-97]++;
+    }
+    for(int i = 0 ; str2[i] !='\0' ; i++){
+        k[str2[i]-97]++;
+    }
+    
+    for(int i = 0 ; i<26 ; i++){
+        if(H[i] != k[i]){
+            return false;
+        }
+    }
+    return true;
 
-    cout << "Vowels = " << vcount << endl;
-    cout << "Consonants = " << ccount << endl;
+}
+bool is_Duplicate_hash(char str[]){
+    int H[26]={0};
+    for(int i = 0 ; str[i] != '\0';i++){
+        H[str[i] - 97]++;
+    }
+    for(int i = 0 ; i<26 ;i++){
+        if(H[i]>1){
+            return true;
+        }
+    }
+    return false;
+}
+bool is_Duplicate_bit(char str[]){
+    int H = 0;
+    int x = 0;
+
+    for(int i = 0; str[i] != '\0'; i++){
+        x = 1;
+        x = x << (str[i] - 'a');
+
+        if((x & H) > 0){
+            return true;
+        }
+
+        H = H | x;
+    }
+
+    return false;
 }
 int main(){
-char* str=create();
-Upper_Lower(str);
+char *str= create();
+// Upper_Lower(str);
+Toggle(str);
+Display(str);
 return 0;
 }
