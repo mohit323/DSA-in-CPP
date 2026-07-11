c8525b997975f77e057de7ba09d18e137a2831be
Mohit Kunwar <mohitkunwar919@gmail.com>
2026-07-11 13:47:30 +0530

    Add files via upload

---
+++ 06-LinkedList/count.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+struct node{
+    int data;
+    node *next;
+};
+node* create(){
+    node* q = new node;
+    q->next = nullptr;
+    return q;
+}
+node* create_list(int arr[],int n){
+    node* head = create();
+    head->data = arr[0];
+    node* last = head;
+    for(int i = 1; i<n; i++){
+        last->next = create();
+        last = last->next;
+        last->data = arr[i];
+    }
+    return head;
+}
+void Display(node* p){
+    while(p != nullptr){
+        cout<<p->data<<" ";
+        p= p->next;
+    }
+}
+int count_nodes(node* p){
+    int count = 0;
+    while (p != nullptr)
+    {
+        count++;
+        p= p->next;
+    }
+    return count;
+}
+int main(){
+int n;
+cout<<"enter the no of nodes: ";
+cin>>n;
+int* arr = new int[n];
+for(int i = 0 ;i<n;i++){
+    cout<<"enter the "<<i+1<<" element: ";
+    cin>>arr[i];
+}
+node* head = create_list(arr , n);
+Display(head);
+cout<<endl;
+int count = count_nodes(head);
+cout<<count;
+return 0;
+}
